c802b5644be4a53537809845f88899b5a8d37c6d
joserafaelnobrega <jr-dado@hotmail.com>
2019-04-17 17:14:47 -0300

    primeiro commit

---
+++ soma_de_dois_numeros.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+
+    int A; // primeiro numero
+    int B; // segundo numero
+    int X; // resultado da soma 
+
+
+    std::cin >> A ;
+    std::cin >> B ;
+    X = A + B;
+
+    std::cout <<"X = "  << X << std::endl;
+
+    return 0;
+}
